1790a8356e3f03944f18561f33bd1dd7a71ead43
Aritra Manna <161790604+Aritrama12@users.noreply.github.com>
2025-04-23 13:18:46 -0700

    changes commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const app=require('./app')
 const config=require('./config/config')
 const port=config.app.port
-const hostname='127.0.0.1'
 
 
 
-app.listen(port,hostname,()=>{
-    console.log(`My server is http://${hostname}:${port}`)
+
+app.listen(port,()=>{
+    console.log(`My server is http://loclalhost:${port}`)
 })
